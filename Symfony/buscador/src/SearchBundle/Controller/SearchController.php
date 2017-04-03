df2c42a02d33cef36cd869bdd937f632eea144af
Coral <coralwhibley@gmail.com>
2017-04-03 13:25:15 +0200

    Buscador terminado

--- Symfony/buscador/src/SearchBundle/Controller/SearchController.php
+++ Symfony/buscador/src/SearchBundle/Controller/SearchController.php
@@ -7,19 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use SearchBundle\Form\SearchType;
 use SearchBundle\Entity\Search;
+use \Google_Client;
+use \Google_Service_YouTube;
 
 class SearchController extends Controller{
 
 	public function youtubeAction(Request $request){
-        $url = new Search();
-    	$form = $this->createForm(SearchType::class, $url);
+        $busqueda = new Search();
+    	$form = $this->createForm(SearchType::class, $busqueda);
     	$form->handleRequest($request);
 
     	if($form->isValid()){
+    		$client = new Google_Client();
 
-    	}
+			$client->setDeveloperKey("AIzaSyD65SvNxzMgS9DsPnZnHX_J4aPE-mZ3YTk");
+			
+			$service = new Google_Service_YouTube($client);
 
-        return $this->render('SearchBundle:Search:search.html.twig', array('form' => $form->createView()));
+			$searchResponse = $service->search->listSearch('id,snippet', array('q' => $busqueda->getSearch(), 'maxResults' => 8,));
+
+			foreach ($searchResponse['items'] as $searchResult) {
+	            if($searchResult['id']['kind']="youtube#video"){
+	                $videos[]= array("title" => $searchResult['snippet']['title'], "url_img" => $searchResult['snippet']['thumbnails']['default']['url'], "fecha" => $searchResult['snippet']['publisetAt'], "canal"=>$searchResult['snippet']['channelTitle']);
+    			}
+    		}
+    		return $this->render('SearchBundle:Search:search.html.twig', array('form' => $form->createView(),'youtube_videos' => $videos));
+       	}
+        return $this->render('SearchBundle:Search:search.html.twig', array('form' => $form->createView(), 'youtube_videos'=>'false'));
     }   
 }
 ?>
--- Symfony/buscador/src/SearchBundle/Form/SearchType.php
+++ Symfony/buscador/src/SearchBundle/Form/SearchType.php
@@ -7,12 +7,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+//use Symfony\Component\Form\Extension\Core\Type\SubmitType;
  
 class SearchType extends AbstractType{
     public function buildForm(FormBuilderInterface $builder, array $options){
         $builder->add('search', TextType::class, array('label' => 'Vídeo: '));
-        $builder->add('save', SubmitType::class);
+        //$builder->add('save', SubmitType::class);
     }
  
     public function getName(){
