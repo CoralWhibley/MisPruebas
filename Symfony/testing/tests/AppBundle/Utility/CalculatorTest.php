8536e29f62643427ce85d8e0c3a8f1f1791d7637
Coral <coralwhibley@gmail.com>
2017-04-04 13:12:42 +0200

    Testeo

--- Symfony/testing/tests/AppBundle/Utility/CalculatorTest.php
+++ Symfony/testing/tests/AppBundle/Utility/CalculatorTest.php
@@ -6,7 +6,7 @@ use AppBundle\Utility\Calculator;
 class CalculatorTest extends \PHPUnit_Framework_TestCase{
 	 public function testAdd(){
 		$stub = $this->getMockBuilder(Calculator::class)->getMock();
-        $stub->expects($this->once())->method('add')->will($this->returnValue(10));
+        $stub->expects($this->once())->method('add')->will($this->returnValue(6));
 
         $suma = new Calculator();
         $result = $suma->add();
